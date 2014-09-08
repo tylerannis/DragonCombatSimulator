a1db16b6f505a7db8466fde952d7adc8ec7939ff
SeedPaths-User <lessdismissive@email.com>
2014-09-08 12:52:23 -0600

    Finished

--- DragonCombatSimulator/DragonCombatSimulator/Program.cs
+++ DragonCombatSimulator/DragonCombatSimulator/Program.cs
@@ -10,7 +10,7 @@ namespace DragonCombatSimulator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to DragonSlayer! You will be trying to kill a dragon with 3 different attacks. \nFirst you have your sword which you will attack using the command 1. \nNext is magic which does less damage then your sword but always hits, use command 2 to use magic. \nLast you can heal yourself with the command 3. You start with 100 hp and the dragon will start with 200. Good luck.");
+            Console.WriteLine("Welcome to DragonSlayer! You will be trying to kill a dragon with 3 different attacks. \nFirst you have your sword which you will attack using the command 1. \nNext is magic which does less damage then your sword but always hits, use command 2 to use magic. \nLast you can heal yourself with the command 3. You start with 100 hp and the dragon will start with 200. Good luck.\nYou just entered the layer and the dragon attacks! Begin by pressing: 1, 2, or 3");
            //call the function
             DragonSlayer();
             Console.ReadKey();
@@ -21,7 +21,8 @@ namespace DragonCombatSimulator
             int yourHealth = 100;
             int dragonHealth = 200;
          
-            
+            //you can use a random number generator multiple times for different things, you don't need a new random for every time you want a different set of numbers
+
            
            
             bool playing = true;
@@ -37,80 +38,72 @@ namespace DragonCombatSimulator
                 int number = hit.Next(0, 101);
                 if (dragonHealth > 0)
                 {
+                    if (hit.Next(0, 101) < 81)
+                    {
+                        //make a rng for Dragon attack strength
+                        int fire = hit.Next(5, 16);
+                        yourHealth = yourHealth - fire;
+                        Console.WriteLine("The dragon did " + fire + " damage.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("That was close better not let it cook you!");
+                    }
 
                 if (input == "1")
                 {
                     //make random number generator for sword attack strength
-                    Random strength = new Random();
-                    int sword = strength.Next(20, 36);
+                    
+                    int sword = hit.Next(20, 36);
                     if (number < 71)
                     {
                         dragonHealth -= sword;
-                        if (number > 81)
-                        {
-                            //make a rng for Dragon attack strength
-                            Random drag = new Random();
-                            int fire = drag.Next(5, 16);
-                            yourHealth -= fire;
-                            Console.WriteLine("Your health is currently " + yourHealth + " and the dragons health is " + dragonHealth + "!");
-                        }
-                        Console.WriteLine("Nice attack you did " + sword + " amount of damage. The dragon now has " + dragonHealth + " hp left.");
+                    
+                        Console.WriteLine("Nice attack you did " + sword + " damage. The dragon now has " + dragonHealth + " hp left.");
                         Console.WriteLine("You have " + yourHealth + " left.");
                         
                     }
                     else
-	{
+	                 {
                         Console.WriteLine("You missed, better scurry away");
                         Console.WriteLine("You have " + yourHealth + " left.");
-	}
+	                 }
                 }
                 else if (input == "2")
                 {
                     //make rng for magic attack strength
-                    Random magic = new Random();
-                    int mage = magic.Next(10, 16);
+                    
+                    int mage = hit.Next(10, 16);
                     dragonHealth -= mage;
-                    if (number > 81)
-                    {
-                        //make a rng for Dragon attack strength
-                        Random drag = new Random();
-                        int fire = drag.Next(5, 16);
-                        yourHealth -= fire;
-                        Console.WriteLine("Your health is currently " + yourHealth + " and the dragons health is " + dragonHealth + "!");
-                    }
+                  
                     Console.WriteLine("The magic takes an effect of " + mage + " damage. The dragons health is now " + dragonHealth);
                     Console.WriteLine("You have " + yourHealth + " left.");
                 }
                 else  if (input == "3")
                 {
                     //make random number generator for healing
-                    Random health = new Random();
-                    int heal = health.Next(10, 21);
+                   
+                    int heal = hit.Next(10, 21);
                     yourHealth += heal;
-                    if (number > 81)
-                    {
-                        //make a rng for Dragon attack strength
-                        Random drag = new Random();
-                        int fire = drag.Next(5, 16);
-                        yourHealth -= fire;
-                        Console.WriteLine("Your health is currently " + yourHealth + " and the dragons health is " + dragonHealth + "!");
-                    }
+                
                     Console.WriteLine("You're still alive! Your health went up by " + heal + " !");
                     Console.WriteLine("You have " + yourHealth + " left.");
                 }
-                
-                   
+                                
                 }
                 else
                 {
                     playing = false;
                     Console.WriteLine("You got lucky and slew the dragon");
                 }
+           
+               
                 if (yourHealth == 0)
                 {
                     playing = false;
                     Console.WriteLine("As expected, the dragon had you for dinner");
                 }
+              
 
             }
         }
